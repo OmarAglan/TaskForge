6962a7c1030677725826da4419207c744d799b2b
Omar Aglan <omar.aglan91@yahoo.com>
2025-11-04 10:33:11 +0200

    Add barrel files for auth and users modules
    Created index.ts files in the auth and users modules to re-export key components, DTOs, entities, and strategies. This improves import convenience and module organization.

---
+++ backend/src/modules/users/index.ts
@@ -0,0 +1,6 @@
+export * from './users.module';
+export * from './users.service';
+export * from './users.controller';
+export * from './entities/user.entity';
+export * from './dto/update-user.dto';
+export * from './dto/change-password.dto';
