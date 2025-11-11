ffe49c9f34efa6a186fabcd57da4179395a83576
Omar Aglan <omar.aglan91@yahoo.com>
2025-11-11 15:46:29 +0200

    Add ActivityModule to Auth and Users modules
    ActivityModule is now imported into both AuthModule and UsersModule using forwardRef to resolve potential circular dependencies. This enables activity-related features to be accessible within authentication and user management logic.

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { ActivityModule } from '../activity/activity.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    forwardRef(() => ActivityModule),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { ActivityModule } from '../activity/activity.module';
 import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => ActivityModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService], // Export for use in AuthModule
 })
-export class UsersModule {}
+export class UsersModule { }
