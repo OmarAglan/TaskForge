9f5ddbceba13fd9ea4c293d36803bfdb1c73e472
Omar Aglan <omar.aglan91@yahoo.com>
2025-11-11 15:21:51 +0200

    Add activity logging interceptor and activity module exports
    Exported the new activity-logging interceptor in the interceptors index. Added an index file for the activity module to re-export module, service, controller, entities, enums, DTOs, interfaces, and helpers for easier imports.

---
+++ backend/src/modules/activity/index.ts
@@ -0,0 +1,22 @@
+// Module
+export * from './activity.module';
+export * from './activity.service';
+export * from './activity.controller';
+
+// Entities
+export * from './entities/activity-log.entity';
+
+// Enums
+export * from './enums/activity-action.enum';
+export * from './enums/entity-type.enum';
+
+// DTOs
+export * from './dto/create-activity-log.dto';
+export * from './dto/filter-activity-logs.dto';
+
+// Interfaces
+export * from './interfaces/activity.interfaces';
+
+// Helpers
+export * from './helpers/metadata-builder.helper';
+export * from './helpers/activity-analytics.helper';
